7302de9f18ef7662e08fe7ea4bc305f1f4bca0fe
Ahmet Gülden <>
2019-12-22 14:16:42 +0300

    Fix record counts

--- bench_hbase.sh
+++ bench_hbase.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 workloads=("workloada" "workloadb" "workloadc" "workloadd" "workloade" "workloadf")
-operations=("100000" "150000" "200000" "250000" "300000")
+operations=("250000" "500000" "1000000" "5000000" "10000000")
 repeatrun=3
-records=1000000000
-threads=400
+records=300000000
+loadthreads=40
+runthreads=400
 driver="hbase14"
 
 rm -rf BenchmarkLogs
@@ -15,14 +16,14 @@ do
     hbase shell ./hbase_truncate
 
     echo "Loading data for" "$work"
-    ./bin/ycsb load $driver -P workloads/"$work" -p table=usertable -p columnfamily=family -p recordcount=$records -threads 40 -s > BenchmarkLogs/"$work""_load.log"
+    ./bin/ycsb load $driver -P workloads/"$work" -p table=usertable -p columnfamily=family -p recordcount=$records -threads $loadthreads -s > BenchmarkLogs/"$work""_load.log"
     echo "Running tests"
 
     for operation in "${operations[@]}"
     do
         for r in $(seq 1 $repeatrun)
         do
-            ./bin/ycsb run $driver -P workloads/"$work" -p table=usertable -p columnfamily=family -p recordcount=$records -p operationcount=$operation -threads $threads > BenchmarkLogs/"$work""_op_""$operation""_run_""$r"".log"
+            ./bin/ycsb run $driver -P workloads/"$work" -p table=usertable -p columnfamily=family -p recordcount=$records -p operationcount=$operation -threads $runthreads > BenchmarkLogs/"$work""_op_""$operation""_run_""$r"".log"
         done
     done
 done
