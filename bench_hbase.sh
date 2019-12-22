e2930d5f63c10aa0699d67646dd73f19510d361b
Ahmet Gülden <>
2019-12-22 12:52:59 +0300

    Add log folder

--- bench_hbase.sh
+++ bench_hbase.sh
@@ -6,19 +6,23 @@ records=1000000000
 threads=400
 driver="hbase14"
 
+rm -rf BenchmarkLogs
+mkdir BenchmarkLogs
+
 for work in "${workloads[@]}"
 do
     echo "Loading data for" "$work"
-    ./bin/ycsb load $driver -P workloads/"$work" -p table=usertable -p columnfamily=family -p recordcount=$records -threads $threads > "$work""_load.log"
+    ./bin/ycsb load $driver -P workloads/"$work" -p table=usertable -p columnfamily=family -p recordcount=$records -threads $threads > BenchmarkLogs/"$work""_load.log"
     echo "Running tests"
 
     for operation in "${operations[@]}"
     do
         for r in $(seq 1 $repeatrun)
         do
-            ./bin/ycsb run $driver -P workloads/"$work" -p table=usertable -p columnfamily=family recordcount=$records -p operationcount=operation -threads $threads > "$work""_op_""$operation""_run_""$r"".log"
+            ./bin/ycsb run $driver -P workloads/"$work" -p table=usertable -p columnfamily=family recordcount=$records -p operationcount=operation -threads $threads > BenchmarkLogs/"$work""_op_""$operation""_run_""$r"".log"
         done
     done
     #Truncate table and start over
     hbase shell ./hbase_truncate
 done
+
