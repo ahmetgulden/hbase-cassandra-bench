9e4176283fce35f0c997eb28415bad2874b784e7
Ahmet Gülden <>
2019-12-22 11:30:26 +0300

    Set hbase driver to hbase14, and make multiple operation count benchmark

--- bench_hbase.sh
+++ bench_hbase.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 workloads=("workloada" "workloadb" "workloadc" "workloadd" "workloade" "workloadf")
-
+operations=("100000" "150000" "200000" "250000" "300000")
 repeatrun=3
-records=300000000
-operations=10000000
+records=1000000000
 threads=400
-driver="hbase10"
+driver="hbase14"
 
-for work in "${workloads[@]}"
-do
-        echo "Loading data for" $work
-        ./bin/ycsb load $driver -P ./workloads/$work -p columnfamily=cf -p hbase.zookeeper.znode.parent=/hbase-unsecure -p recordcount=$records -threads 40 > $work"_load.log"
-        echo "Running tests"
-        for r in `seq 1 $repeatrun`
-        do
-                ./bin/ycsb run $driver -P ./workloads/$work -p columnfamily=cf -p hbase.zookeeper.znode.parent=/hbase-unsecure -p recordcount=$records -p operationcount=$operations -threads $threads > $work"_run_"$r".log"
+for work in "${workloads[@]}" do
+    echo "Loading data for" $work
+    ./bin/ycsb load $driver -P ./workloads/$work -p table=usertable -p columnfamily=family -p recordcount=$records -threads $threads > $work"_load.log"
+    echo "Running tests"
+
+    for operation in "${operations[@]}" do
+        for r in `seq 1 $repeatrun` do
+            ./bin/ycsb run $driver -P ./workloads/$work -p table=usertable -p columnfamily=family recordcount=$records -p operationcount=operation -threads $threads > $work"_op_"operation"_run_"$r".log"
         done
-        #Truncate table and start over
-        hbase shell ./hbase_truncate
+    done
+    #Truncate table and start over
+    hbase shell ./hbase_truncate
 done
